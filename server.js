e4a314f03b5e5f9d3b1e707453b9ebb422a712cf
Eunsong Kim <songkim.w@gmail.com>
2022-03-26 07:34:23 -0400

    link

--- server.js
+++ server.js
@@ -8,18 +8,18 @@ app.listen(8080, function(){
     console.log('listening on 8080');
 });
 
-app.get('/', function(req, res){
+app.get('/muko', function(req, res){
     res.sendFile(__dirname + '/index.html')
 });
 
-app.get('muko/rooms', function(req, res){
+app.get('/muko/rooms', function(req, res){
     res.render('rooms.ejs');
 });
 
-app.get('muko/about', function(req, res){
+app.get('/muko/about', function(req, res){
     res.render('about.ejs');
 });
 
-app.get('muko/amenities', function(req, res){
+app.get('/muko/amenities', function(req, res){
     res.render('amenities.ejs');
 });
